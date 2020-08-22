8da6749f6744775c7de553a6112efc8afd0c6c25
Darkwah Boahen (Sam) <sdboahen@gmail.com>
2020-08-22 00:27:04 +0000

    Done.

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,6 +1,77 @@
 # prereqs: iterators, hashes, conditional logic
 # Given a hash with numeric values, return the key for the smallest value
 
-def key_for_min_value(name_hash)
+def 
+  
+  key_for_min_value(name_hash)
 
+
+  puts "(name_hash):: {|| #{name_hash} ||}"
+  
+    
+  lowest_value = 400000000000000000  #0.00
+  lowest_key   = nil
+  # -x-  lowest_key   = :somekey    ~
+
+
+  puts "(lowest_value):: {|| #{lowest_value} ||}
+  
+  LEHDO-EHYT !
+
+"
+  puts puts puts puts
+  
+
+   #                 ***
+  #
+  name_hash.each do |product, value|  # X |  (X__price)
+    
+  if  lowest_value > value
+      # -X- (<) // I D K . . . >_> but it works now :I //
+      # {...}(>=)
+
+      lowest_key    = product
+      lowest_value  = value  end
+
+          ## -T- ##
+          puts "(lowest_key)::   {|| #{lowest_key} ||}  ***"
+          ## -T- ##
+                #  XX-  lowest_key    = product
+
+                 end
+  #
+  #                 ***
+    puts
+    puts "(lowest_key)::   {|| #{lowest_key} ||}  ***"
+    puts "(lowest_value):: {|| #{lowest_value} ||}  ***
+    
+    
+    ----------------------------------------
+    ----------------------------------------
+    
+    "
+    puts puts puts puts
+  
+  lowest_key
+  # X |  lowest_value
+
+  
 end
+
+
+
+
+
+
+
+# {...}
+# ikea = {:chair => 25, :table => 85, :mattress => 450}
+# key_for_min_value(ikea)
+# # => :chair
+ 
+# veggies = {"apple" => -45, "banana" => -44.5, "carrot" => -44.9}
+# key_for_min_value(veggies)
+# # => "apple"
+
+
+
